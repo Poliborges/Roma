63433e6bb2a942fd7edd2041d8f0530b3f76383c
polianaborges <polianaborgesdev@gmail.com>
2024-01-17 14:44:48 -0300

    Dados mokados e funcionalidade de filtro de busca

--- Roma/src/App.tsx
+++ Roma/src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 const frutas = ["banana", "manga", "uva", "pera", "tomate", "caju"];
 
 export default function App() {
+  const [busca, setBusca] = useState("");
+  const frutasfiltradas = frutas.filter((fruta) =>
+    fruta.toLowerCase().includes(busca.toLowerCase())
+  );
+
   return (
     <div className="App">
       <h1>Minhas frutas</h1>
-      <input type="text" />
+      <input
+        type="text"
+        value={busca}
+        onChange={(ev) => setBusca(ev.target.value)}
+      />
+
       <ul>
-        {" "}
-        {frutas.map((fruta) => (
+        {frutasfiltradas.map((fruta) => (
           <li key={fruta}> {fruta}</li>
         ))}
       </ul>
